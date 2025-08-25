736a43d609baa26e5f155c2c84a6c500fac5427a
Michael-horse <atharva8sinha6@gmail.com>
2025-08-25 10:54:20 +0000

    done

---
+++ js/data_types.js
@@ -0,0 +1,20 @@
+"use strict";//treat all code as newer version
+
+// alert("Hello") this works on browser but not on nodejs
+
+let name="ac"
+let age=21
+let alive=null
+let a;
+//number => range is about 2^53
+//bigint
+//string=> can be used using" " or ' '
+//boolean=>true/false
+//null=> standalone value
+//undefined=>when var declared but not initialized
+//symbol=>unique
+
+//object
+
+console.log(typeof a)//undefined
+console.log(typeof null)//object type
---
+++ js/variables.js
@@ -0,0 +1,9 @@
+const accid=1232
+let email="ac@gmail.com"
+var accpass="23312"
+city="scsa"
+// accid=24 not allowed
+console.log(accid)
+email="sa"
+accpass="q23"
+console.table([accid,email,accpass,city ])
