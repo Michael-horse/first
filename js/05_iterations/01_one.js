803f4d86fd37128068a2cd950cf766d33a844cce
Michael-horse <atharva8sinha6@gmail.com>
2025-09-02 18:18:29 +0000

    loops

---
+++ js/05_iterations/01_one.js
@@ -0,0 +1,21 @@
+//for loop
+
+// for (let i= 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element)
+    
+// }
+
+// let arr=["reatrd","kds","kdv"]
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[index]);
+    
+// }
+
+//break and continue
+
+for (let index = 1; index <=20; index++) {
+    if(index==5)
+        break;
+    console.log(index)
+}
---
+++ js/05_iterations/02_two.js
@@ -0,0 +1,17 @@
+
+let index=0;
+while (index<=10) {
+    console.log(`value ofindex is ${index}`)
+    index++
+}
+let arr=['aqua','ape','afk']
+let l=0
+// while(l<arr.length)
+// {
+//     console.log(`val is ${arr[l]}`)
+//     l++
+// }
+do{
+console.log(l++)
+}
+while(l<=10)
